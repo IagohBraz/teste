6d623ff7d982afadb78a6a95034bccd3c383d021
Iagoh Braz <contato@iagobraz.com>
2024-08-28 08:15:06 -0300

    teste

---
+++ main.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome: ")
+
+salario = float(input("Digite seu salario: "))
+
+bonus = float(input("Digite seu bonus: "))
+
+valor_final = salario + salario * bonus
+
+print(f"Parabens {nome}, o valor a receber esse mês é de {valor_final}")
+
+#fim
+
+
+
+
+
+
